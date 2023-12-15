5cb0a21042fa5303ce6ad4d796d17f14030181f5
Jay <itexlzvisual@gmail.com>
2023-12-15 20:27:37 +1030

    delete ikon projects (not optmised)

---
+++ resources/ez-fishing/client/peds.lua
@@ -0,0 +1,101 @@
+-- -- --============================================================== Exports
+-- exports[Config.targetExport]:AddTargetModel(`s_m_y_ammucity_01`, {
+--     options = {
+--         {
+--             event = "ez-fishing:client:BoatMenu",
+--             icon = "fas fa-anchor",
+--             label = "Boat Rental",
+--             location = 1 --LaPuerta
+--         },
+-- 		{
+--             event = "ez-fishing:client:buyFishingGear",
+--             icon = "fas fa-fish",
+--             label = "Fishing Gear",
+--         },
+--     },
+--     distance = 10.0
+-- })
+
+-- exports[Config.targetExport]:AddTargetModel(`u_m_m_filmdirector`, {
+--     options = {
+--         {
+--             event = "ez-fishing:client:BoatMenu",
+--             icon = "fas fa-anchor",
+--             label = "Boat Rental",
+--             location = 2 --PaletoCove
+--         },
+-- 		{
+--             event = "ez-fishing:client:buyFishingGear",
+--             icon = "fas fa-fish",
+--             label = "Fishing Gear",
+--         },
+--     },
+--     distance = 10.0
+-- })
+
+-- exports[Config.targetExport]:AddTargetModel(`s_m_o_busker_01`, {
+--     options = {
+--         {
+--             event = "ez-fishing:client:BoatMenu",
+--             icon = "fas fa-anchor",
+--             label = "Boat Rental",
+--             location = 3 --ElGordo
+--         },
+-- 		{
+--             event = "ez-fishing:client:buyFishingGear",
+--             icon = "fas fa-fish",
+--             label = "Fishing Gear",
+--         },
+--     },
+--     distance = 10.0
+-- })
+
+-- exports[Config.targetExport]:AddTargetModel(`ig_cletus`, {
+--     options = {
+--         {
+--             event = "ez-fishing:client:BoatMenu",
+--             icon = "fas fa-anchor",
+--             label = "Boat Rental",
+--             location = 4 --ActDam
+--         },
+-- 		{
+--             event = "ez-fishing:client:buyFishingGear",
+--             icon = "fas fa-fish",
+--             label = "Fishing Gear",
+--         },
+--     },
+--     distance = 10.0
+-- })
+
+-- exports[Config.targetExport]:AddTargetModel(`a_m_m_hillbilly_01`, {
+--     options = {
+--         {
+--             event = "ez-fishing:client:BoatMenu",
+--             icon = "fas fa-anchor",
+--             label = "Boat Rental",
+--             location = 5 --AlamoSea
+--         },
+-- 		{
+--             event = "ez-fishing:client:buyFishingGear",
+--             icon = "fas fa-fish",
+--             label = "Fishing Gear",
+--         },
+--     },
+--     distance = 10.0
+-- })
+
+-- exports[Config.targetExport]:AddTargetModel(`s_m_y_busboy_01`, {
+--     options = {
+--         {
+--             event = "ez-fishing:client:SellLegalFish",
+--             icon = "fa fa-fish",
+--             label = "Sell Fish",
+--         },
+-- 		{
+--             event = "ez-fishing:client:SellillegalFish",
+--             icon = "fa fa-fish",
+--             label = "Sell Exotic Fish",
+--         },
+--     },
+--     distance = 10.0
+-- })

bff3bd57bcd197b59f2ae031f2cb93921c474753
Ordinary <georgeebdonyt@gmail.com>
2023-12-18 23:32:27 +0000

    Some more gear tweaks

--- resources/[george]/qb-gears/gears.lua
+++ resources/[george]/qb-gears/gears.lua
@@ -38,15 +38,18 @@ CreateThread(function()
     end
 end)
 
+local orgtopspeed = nil
+
 RegisterNetEvent('OD-Gears:EnteredVehicle', function(vehicle)
     if IsPedInAnyVehicle(PlayerPedId(), false) then
         vehicle = GetVehiclePedIsIn(PlayerPedId(),false)
-        vehicles_max_gears = GetVehicleHandlingFloat(vehicle, "CHandlingData", "nInitialDriveGears")
         vehicles_Gear = GetVehicleCurrentGear(vehicle)
         if vehicle ~= CurrentHash then
-            originaltopspeed = GetVehicleHandlingFloat(vehicle, "CHandlingData", "fInitialDriveMaxFlatVel")
+            vehicles_max_gears = GetVehicleHandlingFloat(vehicle, "CHandlingData", "nInitialDriveGears")
             hbrake = GetVehicleHandlingFloat(vehicle, "CHandlingData", "fHandBrakeForce")
+            orgtopspeed = GetVehicleHandlingFloat(vehicle, "CHandlingData", "fInitialDriveMaxFlatVel")
         end
+        originaltopspeed = tonumber(orgtopspeed)
     end
 end)
 
@@ -77,7 +80,9 @@ end
 
 function ShiftDown()
     if Current_Gear > 0 then
-        Current_Gear = Current_Gear - 1
+        if Current_Gear > vehicles_Gear then
+            Current_Gear = Current_Gear - 1
+        end
     else
         return
     end
@@ -85,60 +90,41 @@ function ShiftDown()
 end
 
 CreateThread(function()
-    while true do 
+    while true do
         Wait(1)
         local vehicle = GetVehiclePedIsIn(PlayerPedId(),false)
         local Vehicle_speed = GetEntitySpeed(vehicle)
         if IsPedInAnyVehicle(PlayerPedId(), false) then
-            if IsControlPressed(0, 76) == false then
-                SetVehicleHandlingFloat(vehicle, "CHandlingData", "fHandBrakeForce", 0.0)
-            else
-                SetVehicleHandlingFloat(vehicle, "CHandlingData", "fHandBrakeForce", hbrake)
-            end
             if Current_Gear ~= 0 and Current_Gear ~= 1 and Current_Gear ~= 2 and Vehicle_speed < 10.0 then
                 SetVehicleHighGear(vehicle,1)
-                ModifyVehicleTopSpeed(vehicle,17)
                 SetVehicleEngineOn(vehicle, false, true, true)
             end
             if Current_Gear == 0 and vehicles_Gear == 0 then
                 SetVehicleHandbrake(vehicle, true)
-                SetVehicleHighGear(vehicle,2)
+                SetVehicleHighGear(vehicle,1)
             elseif Current_Gear == 1 and vehicles_max_gears > 1 and vehicles_Gear == 1 then
-                SetVehicleHandlingFloat(vehicle, "CHandlingData", "fInitialDriveMaxFlatVel", originaltopspeed/6.4)
-                ModifyVehicleTopSpeed(vehicle,30)
-                SetVehicleHighGear(vehicle,3)
+                SetVehicleHandbrake(vehicle, false)
+                SetVehicleHighGear(vehicle,2)
             elseif Current_Gear == 2 and vehicles_max_gears > 2 and vehicles_Gear == 2 then
-                SetVehicleHandlingFloat(vehicle, "CHandlingData", "fInitialDriveMaxFlatVel", originaltopspeed/6.4)
-                ModifyVehicleTopSpeed(vehicle,30)
+                -- SetVehicleHandlingFloat(vehicle, "CHandlingData", "fInitialDriveMaxFlatVel", originaltopspeed*0.65)
                 SetVehicleHighGear(vehicle,3)
             elseif Current_Gear == 3 and vehicles_max_gears > 3 and vehicles_Gear == 3 then
-                SetVehicleHandlingFloat(vehicle, "CHandlingData", "fInitialDriveMaxFlatVel", originaltopspeed/6.7)
-                ModifyVehicleTopSpeed(vehicle,50)
+                -- SetVehicleHandlingFloat(vehicle, "CHandlingData", "fInitialDriveMaxFlatVel", originaltopspeed*0.45)
                 SetVehicleHighGear(vehicle,4)
             elseif Current_Gear == 4 and vehicles_max_gears > 4 and vehicles_Gear == 4 then
-                SetVehicleHandlingFloat(vehicle, "CHandlingData", "fInitialDriveMaxFlatVel", originaltopspeed/6.3)
-                ModifyVehicleTopSpeed(vehicle,60)
+                -- SetVehicleHandlingFloat(vehicle, "CHandlingData", "fInitialDriveMaxFlatVel", originaltopspeed)
                 SetVehicleHighGear(vehicle,5)
             elseif Current_Gear == 5 and vehicles_max_gears > 5 and vehicles_Gear == 5 then
-                SetVehicleHandlingFloat(vehicle, "CHandlingData", "fInitialDriveMaxFlatVel", originaltopspeed/5.7)
-                ModifyVehicleTopSpeed(vehicle,80)
                 SetVehicleHighGear(vehicle,6)
-            elseif Current_Gear == 6 and vehicles_max_gears > 6 and vehicles_Gear == 6 then
-                SetVehicleHandlingFloat(vehicle, "CHandlingData", "fInitialDriveMaxFlatVel", originaltopspeed/1.2)
-                ModifyVehicleTopSpeed(vehicle,80)
+            elseif Current_Gear == 6 and vehicles_max_gears > 6 and vehicles_Gear == 7 then
                 SetVehicleHighGear(vehicle,7)
-            elseif Current_Gear == 7 and vehicles_max_gears > 7 and vehicles_Gear == 7 then
-                SetVehicleHandlingFloat(vehicle, "CHandlingData", "fInitialDriveMaxFlatVel", originaltopspeed)
-                ModifyVehicleTopSpeed(vehicle,80)
+            elseif Current_Gear == 7 and vehicles_max_gears > 7 and vehicles_Gear == 8 then
                 SetVehicleHighGear(vehicle,8)
-            elseif Current_Gear == 8 and vehicles_max_gears > 8 and vehicles_Gear == 8 then
-                ModifyVehicleTopSpeed(vehicle,80)
+            elseif Current_Gear == 8 and vehicles_max_gears > 8 and vehicles_Gear == 9 then
                 SetVehicleHighGear(vehicle,9)
-            elseif Current_Gear == 9 and vehicles_max_gears > 9 and vehicles_Gear == 9 then
-                ModifyVehicleTopSpeed(vehicle,80)
+            elseif Current_Gear == 9 and vehicles_max_gears > 9 and vehicles_Gear == 10 then
                 SetVehicleHighGear(vehicle,10)
-            elseif Current_Gear == 1 and vehicles_max_gears > 10 and vehicles_Gear == 10 then
-                ModifyVehicleTopSpeed(vehicle,80)
+            elseif Current_Gear == 10 and vehicles_max_gears > 10 and vehicles_Gear == 11 then
                 SetVehicleHighGear(vehicle,11)
             end
         end
